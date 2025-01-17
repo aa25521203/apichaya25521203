41f26dea440277255e230e5d8f943aaf2370fa8d
aa25521203 <24521@srithatpit.ac.th>
2025-01-17 11:29:00 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>เบื้องต้น</title>
+</head>
+<body>
+   <p>------การแสดงข้อความแบบหัวข้อ------</p>
+   <div>
+      <H1>HTMLเบื้องต้น</H1>
+   </div>
+      <H2>HTMLเบื้องต้น</H2>
+      <H3>HTMLเบื้องต้น</H3>
+      <H4>HTMLเบื้องต้น</H4>
+      <H5>HTMLเบื้องต้น</H5>
+      <H6>HTMLเบื้องต้น</H6>
+   </div>
+   <p>------การสร้างตัวหนา ตัวเอียง การขีดเส้นใต้ข้อความ------</p>
+   <div>
+      <b>อภิชญา ภูพวก</b><br>
+      <i>อภิชญา ภูพวก</i><br>
+      <u>อภิชญา ภูพวก</u><br>
+   </div>
+   <p><b>-----การ Link------</b></p>
+   <div>
+      <a href = "www.google.co.th">Google</a><br>
+   </div>
+</body>
+</html>
